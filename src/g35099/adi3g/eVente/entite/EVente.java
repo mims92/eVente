72d727a8c0187dc981c67e3c948f7be53ea52f9d
Florian <Florian@192.168.0.10>
2013-10-02 20:32:37 +0200

    Premier commit

---
+++ src/g35099/adi3g/eVente/entite/EVente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g35099.adi3g.eVente.entite;
+
+/**
+ *
+ * @author Florian
+ */
+public class EVente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
